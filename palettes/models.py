87eac356dd42b815700b4becd6681839ba5a33e4
Sudipto Roy <sudiptoroy1122@gmail.com>
2020-01-11 15:12:10 +0600

    Add model Palettes and insert data

--- palettes/models.py
+++ palettes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Table for storing palettes info
+class Palettes(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- palettes/views.py
+++ palettes/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Palettes
 
 # Create your views here.
 
 # Dummy data
-palettes = [
+"""palettes = [
     {
         'author' : 'Sudipto Roy',
         'title' : 'Gray Accent',
-        'content' : 'Colors',
+        'content' : '#32a852,#32a852,#32a852,#32a852,#32a852,#32a852',
         'date_posted' : 'January 11, Saterday'
     },
     {
         'author' : 'Jane Doe',
         'title' : 'Red Accent',
-        'content' : 'Colors',
+        'content' : '#32a852,#32a852,#32a852,#32a852,#32a852,#32a852',
         'date_posted' : 'January 11, Saterday'
     } 
-]
+]"""
 
 def home(request):
     """Home page view for palettes app"""
 
     # Import dummy data
     context = {
-        'palettes_content' : palettes
+        'palettes_content' : Palettes.objects.all()
     }
     return render(request, 'palettes/home.html', context)
 
